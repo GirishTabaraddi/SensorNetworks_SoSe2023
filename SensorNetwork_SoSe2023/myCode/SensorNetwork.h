6dba05d6cfaf757f7907036b154b1802af69fc7e
Girish Tabaraddi <girishtabaraddi@gmail.com>
2024-02-09 23:57:35 +0100

    09-Feb-24: adding repo of SoSe2023 paper

---
+++ SensorNetwork_SoSe2023/myCode/SensorNetwork.h
@@ -0,0 +1,33 @@
+#ifndef SENSORNETWORK_H_
+#define SENSORNETWORK_H_
+
+#include <list>
+#include <memory>
+#include "Sensor.h"
+
+/**
+ * Models a network of sensors.
+ */
+class SensorNetwork {
+private:
+    std::list<std::unique_ptr<Sensor>> sensors;
+
+public:
+    /**
+     * Adds the given sensor to the network. If a sensor with
+     * the name of the sensor passed as parameter already
+     * exists in the network, throws a DuplicateSensorName
+     * exception. (11 points)
+     *
+     * @param sensor the sensor to add
+     * @throws DuplicateSensorName
+     */
+    SensorNetwork& add(std::unique_ptr<Sensor>&& sensor);
+
+	const std::list<std::unique_ptr<Sensor> >& getSensors() const
+	{
+		return sensors;
+	}
+};
+
+#endif /* SENSORNETWORK_H_ */
---
+++ SensorNetwork_SoSe2023/myCode/TestSensor.cpp
@@ -0,0 +1,19 @@
+#include "TestSensor.h"
+
+using namespace std;
+
+TestSensor::TestSensor(const std::string &name,
+		std::shared_ptr<Adjuster> adjustor) : Sensor(name, adjustor), value(0.0)
+{
+
+}
+
+void TestSensor::setRawReading(float value)
+{
+	this->value = value;
+}
+
+float TestSensor::rawReading() const
+{
+	return this->value;
+}
